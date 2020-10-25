33f6d21289e34876877423ccab97cbe130a0fbb8
baruna <baruna@NEBULA>
2020-10-25 22:21:06 +0700

    save

---
+++ RentalKendaraan_014/Models/JenisKendaraan.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan_014.Models
+{
+    public partial class JenisKendaraan
+    {
+        public int IdJenisKendaraan { get; set; }
+        public string NamaJenisKendaraan { get; set; }
+    }
+}
---
+++ RentalKendaraan_014/Models/Kendaraan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan_014.Models
+{
+    public partial class Kendaraan
+    {
+        public int IdKendaraan { get; set; }
+        public string NamaKendaraan { get; set; }
+        public string N0Polisi { get; set; }
+        public string N0Stnk { get; set; }
+        public int? IdJenisKendaraan { get; set; }
+        public string Ketersediaan { get; set; }
+    }
+}
